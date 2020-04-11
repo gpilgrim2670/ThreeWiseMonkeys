2f84860b8711341eb41fbde6460e5a751b83672d
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-04-11 16:00:59 -0400

    tests for Hear

--- tests/testthat/test-Hear.R
+++ tests/testthat/test-Hear.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(stringr)
+
+test_that("Here works string", {
+  expect_match(Hear("evil"), "I can't hear you!")
+})
+
+test_that("Here works string 2", {
+  expect_match(Hear("no- _evil"), "I hear you!")
+})
+
+test_that("Here works value", {
+  evil <- "asdf"
+  expect_match(Hear(evil), "I can't hear you!")
+})
+
+test_that("Here works value 2", {
+  no_evil <- "asdf"
+  expect_match(Hear(no_evil), "I hear you!")
 })
