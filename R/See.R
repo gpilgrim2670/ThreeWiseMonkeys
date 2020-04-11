2a224f8e8d75e5d58932b25ee8b319d65baaebc5
Greg Pilgrim <gpilgrim2670@gmail.com>
2020-04-11 12:55:10 -0400

    check passed

--- R/See.R
+++ R/See.R
@@ -1,14 +1,14 @@
 #' see no evil
 #'
-#' @param x an input
+
 #' @return A plot depicting \code{"No Evil."}.
 #'
 #' @export
 #'
 #' @examples
-#' See(1)
+#' See()
 
-See <- function(x) {
+See <- function() {
   plot(1, 10, type = "n", xaxt = "n", yaxt = "n", ann = FALSE, axes = F)
   text(1, 10, "No Evil.", cex = 7)
 }
--- R/Speak.R
+++ R/Speak.R
@@ -1,17 +1,16 @@
 #' speak no evil
 #'
 #' @importFrom tuneR play
-#' @param x an input
 #' @return Plays an audio clip of the words "No Evil.".
 #'
 #' @export
 #'
 #' @examples \dontrun{
-#' Hear(1)
+#' Speak()
 #' }
 #'
 
-Speak <- function(x) {
+Speak <- function() {
   file_path <- system.file("audio", "No_Evil.wav", package = "ThreeWiseMonkeys")
   # file_path <- here::here("audio", "No_Evil.wav")
   # tuneR::setWavPlayer("/Applications/'QuickTime Player.app'/Contents/MacOS/'QuickTime Player'")
